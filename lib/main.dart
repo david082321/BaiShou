2f53e5a593be1e5a2137554d30f023f5c6ce7721
TenkouAkatsuki <alice_kei@163.com>
2026-02-22 04:33:23 +0800

    性能优化 (剔除 Markdown 渲染) 与日期颜色修正。 统一卡片的时间戳颜色。 侧边栏宽度、间距及文案优化。 强制窗口最小尺寸 1200x800。

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:window_manager/window_manager.dart';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting();
+
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    await windowManager.ensureInitialized();
+    WindowOptions windowOptions = const WindowOptions(
+      minimumSize: Size(1200, 800),
+      center: true,
+      title: '白守 - 记录你珍贵的回忆',
+    );
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+    });
+  }
 
   final prefs = await SharedPreferences.getInstance();
 
